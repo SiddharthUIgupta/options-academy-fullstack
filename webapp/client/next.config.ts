7ec85878c8a0e4a6703567c47522d565e28a0675
SiddharthUIgupta <siddharthgupta3391@gmail.com>
2026-05-21 22:34:53 -0700

    Fix Next.js basePath for new repository name

--- webapp/client/next.config.ts
+++ webapp/client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/options-academy',
+  basePath: '/options-academy-fullstack',
   images: {
     unoptimized: true,
   },
